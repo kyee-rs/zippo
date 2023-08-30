130770bba6334a4039d74a22e344de8654870b46
Saputskyi Petro <hello@lowt.live>
2023-08-30 17:46:23 +0300

    feat: added dialogues

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
-use std::io::Cursor;
-use std::path::PathBuf;
-use std::{env, fs};
+use anyhow::anyhow;
+use inquire::{Confirm, Text};
+use std::env;
+use std::fs::File;
+use zip::ZipArchive;
 
 fn main() -> anyhow::Result<()> {
-    let archive = fs::read(env::current_exe()?)?;
-    let target_dir = PathBuf::from(".");
-    zip_extract::extract(Cursor::new(archive), &target_dir, true)?;
+    let archive = File::open(env::current_exe()?)?;
+    let mut archive = ZipArchive::new(archive)?;
 
-    Ok(())
+    for idx in 0..archive.len() {
+        let entry = archive.by_index(idx)?;
+        if entry.is_file() {
+            let name = entry
+                .enclosed_name()
+                .ok_or(anyhow!(
+                    "Failed to read the content. This archive is potentially unsafe."
+                ))?
+                .to_string_lossy()
+                .to_string();
+
+            println!("- {name}")
+        }
+    }
+
+    let confirm = Confirm::new("Are you sure you want to extract this archive? ")
+        .with_default(true)
+        .with_help_message(
+            "This archive was bundled using Grizzly v0.1.2-rc-2 (https://github.com/12subnet/grizzly)"
+        )
+        .prompt();
+    let path = Text::new("Extract to: ")
+        .with_default(".")
+        .with_placeholder(".")
+        .prompt()?;
+
+    match confirm {
+        Ok(true) => {
+            println!("Extracting to {}...", path);
+            archive.extract(path)?;
+
+            Ok(())
+        }
+        Ok(false) => Ok(()),
+        Err(_) => Err(anyhow!("Failed to read your input.")),
+    }
 }
